382a312fb9f5af7546ef9efbd085f0f13308c480
ClxS <slxxls92@gmail.com>
2016-03-17 19:31:14 +0000

    Cleaned up unused "Using's" in the debugger

--- RevolutionUnitTests/StardewRevolutionLauncher.cs
+++ RevolutionUnitTests/StardewRevolutionLauncher.cs
@@ -1,9 +1,5 @@
-﻿using StardewValley;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System;
 using System.Reflection;
-using System.Text;
 
 namespace RevolutionDebugger
 {
