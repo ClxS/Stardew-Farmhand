12e38cb8170dfb9dcaac11a195ee4e7914a8928a
ClxS <slxxls92@gmail.com>
2016-03-25 12:23:00 +0000

    Small cleanp to reduce nesting

--- Libraries/Revolution/Content/ContentManager.cs
+++ Libraries/Revolution/Content/ContentManager.cs
@@ -30,28 +30,27 @@ namespace Revolution.Content
         public override T Load<T>(string assetName)
         {
             var item = XnbRegistry.GetItem(assetName);
-            if(item?.OwningMod != null && item.OwningMod.ModState == ModState.Loaded)
-            {
-                try
-                {
-                    var currentDirectory = Path.GetDirectoryName(item.AbsoluteFilePath);
-                    var relPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "\\" +
-                                   RootDirectory + "\\";
-                    if (currentDirectory != null)
-                    {
-                        var relRootUri = new Uri(relPath, UriKind.Absolute);
-                        var fullPath = new Uri(currentDirectory, UriKind.Absolute);
-                        var relUri = relRootUri.MakeRelativeUri(fullPath) + "/" + item.File;
+            if (item?.OwningMod?.ModState == null || item.OwningMod.ModState != ModState.Loaded) return base.Load<T>(assetName);
 
-                        Log.Verbose($"Using own asset replacement: {assetName} = {relPath}");
-                        return base.Load<T>(relUri);
-                    }
-                }
-                catch (Exception ex)
+            try
+            {
+                var currentDirectory = Path.GetDirectoryName(item.AbsoluteFilePath);
+                var relPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "\\" +
+                              RootDirectory + "\\";
+                if (currentDirectory != null)
                 {
-                    Log.Exception("Error reading own file", ex);
-                    return base.Load<T>(assetName);
+                    var relRootUri = new Uri(relPath, UriKind.Absolute);
+                    var fullPath = new Uri(currentDirectory, UriKind.Absolute);
+                    var relUri = relRootUri.MakeRelativeUri(fullPath) + "/" + item.File;
+
+                    Log.Verbose($"Using own asset replacement: {assetName} = {relPath}");
+                    return base.Load<T>(relUri);
                 }
+            }
+            catch (Exception ex)
+            {
+                Log.Exception("Error reading own file", ex);
+                return base.Load<T>(assetName);
             }
             return base.Load<T>(assetName);
         }
