94268608f14ed8ea6115b62df63fb7aef73a7aee
ClxS <slxxls92@gmail.com>
2016-04-06 19:45:34 +0100

    Started work on the API layer, starting with the player

---
+++ Libraries/Farmhand/API/CraftingRecipe.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using StardewValley;
+
+namespace Farmhand.API
+{
+    public class CraftingRecipe
+    {
+        public int Id { get; set; }
+        public int PrivateId { get; private set; } = -1;
+        public bool IsAdded => PrivateId != -1;
+        public bool IsUnlocked => false; //TODO
+
+        public string Name { get; set; } = string.Empty;
+        public SerializableDictionary<string, int> RequiredSkills = new SerializableDictionary<string, int>(); 
+        public RecipeUnlockType RecipeUnlockType = RecipeUnlockType.SkillBased;
+    }
+}
---
+++ Libraries/Farmhand/API/Player.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using StardewValley;
+
+namespace Farmhand.API
+{
+    /// <summary>
+    /// API layer to interact with the player
+    /// </summary>
+    public static class Player
+    {
+        /// <summary>
+        /// Contains all custom crafting recipes added to the API
+        /// </summary>
+        public static List<CraftingRecipe> CraftingRecipes = new List<CraftingRecipe>();
+         
+        /// <summary>
+        /// Adds custom save-safe crafting recipes
+        /// </summary>
+        /// <param name="recipe">The recipe to add</param>
+        /// <param name="farmer">The farmer to add. Will default to Game1.player when null</param>
+        /// <returns>The internal id assigned to this recipe</returns>
+        public static int AddRecipe(CraftingRecipe recipe, Farmer farmer = null)
+        {
+            if (farmer == null)
+            {
+                farmer = Game1.player;
+            }
+            
+            return 0;
+        }
+    }
+}
