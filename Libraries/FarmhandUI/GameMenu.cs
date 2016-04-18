eab5176386cb08ed887c71b3dde1197eb764e85a
ClxS <slxxls92@gmail.com>
2016-04-18 18:47:51 +0100

    Early work into extra menu tabs for #5, still needs testing and making sure rendering works

---
+++ Libraries/FarmhandUI/GameMenu.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using Farmhand.Attributes;
+using StardewValley.Menus;
+
+namespace Farmhand.UI
+{
+    [HookRedirectConstructorFromBase("StardewValley.Game1", "Window_ClientSizeChanged")]
+    [HookRedirectConstructorFromBase("StardewValley.Game1", "updateActiveMenu")]
+    [HookRedirectConstructorFromBase("StardewValley.Game1", "checkForEscapeKeys")]
+    [HookRedirectConstructorFromBase("StardewValley.Game1", "UpdateControlInput")]
+    [HookRedirectConstructorFromBase("StardewValley.Game1", "UpdateControlInput")]
+    [HookRedirectConstructorFromBase("StardewValley.GameLocation", "openCraftingMenu")]
+    [HookRedirectConstructorFromBase("StardewValley.Menus.GameMenu", "receiveGamePadButton")]
+    [HookRedirectConstructorFromBase("StardewValley.Menus.MenuHUDButtons", "receiveLeftClick")]
+    [HookRedirectConstructorFromBase("StardewValley.Menus.OptionsInputListener", "receiveLeftClick")]
+    [HookRedirectConstructorFromBase("StardewValley.Options", "setWindowedOption")]
+    [HookRedirectConstructorFromBase("StardewValley.Options", "changeDropDownOption")]
+    public class GameMenu : StardewValley.Menus.GameMenu
+    {
+        public ReadOnlyCollection<IClickableMenu> Pages => pages.AsReadOnly();
+        public ReadOnlyCollection<ClickableComponent> Tabs => tabs.AsReadOnly();
+
+        public GameMenu()
+        {
+        }
+
+        public GameMenu(int startingTab, int extra = -1)
+        : base(startingTab, extra)
+        { }
+
+        public void AddPage(IClickableMenu menu)
+        {
+            this.pages.Add(menu);
+        }
+
+        public void RemovePage(IClickableMenu menu)
+        {
+            this.pages.Remove(menu);
+        }
+
+        public void AddTab(ClickableComponent tab)
+        {
+            this.tabs.Add(tab);
+        }
+
+        public void RemoveTab(ClickableComponent tab)
+        {
+            this.tabs.Remove(tab);
+        }
+    }
+}
