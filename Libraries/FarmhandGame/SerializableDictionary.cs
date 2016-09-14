f6f68539698fd38fc530e92bc6e6ada01b29001e
TownEater <TownEater@gmail.com>
2016-09-14 14:55:11 -0600

    FhSerializableDictionary Hooks
    - Hooked all but one insantiation of SerializableDictionary to use
    FhSerializableDictionary Instead. I could not get the one remaining
    instance to work due to the complexity of its full name.
    - Removed FhSerializableDictionary constructor debug message

--- Libraries/FarmhandGame/SerializableDictionary.cs
+++ Libraries/FarmhandGame/SerializableDictionary.cs
@@ -10,14 +10,35 @@ using System.Xml.Serialization;
 
 namespace Farmhand.Overrides.Game
 {
+    [HookRedirectConstructorFromBase("StardewValley.AnimalHouse", ".ctor", typeof(long), typeof(StardewValley.FarmAnimal))]
+    [HookRedirectConstructorFromBase("StardewValley.AnimalHouse", "System.Void StardewValley.AnimalHouse::.ctor(xTile.Map,System.String)", typeof(long), typeof(StardewValley.FarmAnimal))]
+    [HookRedirectConstructorFromBase("StardewValley.Buildings.Building", "getIndoors", typeof(Vector2), typeof(StardewValley.TerrainFeatures.TerrainFeature))]
+    [HookRedirectConstructorFromBase("StardewValley.Buildings.Building", "load", typeof(Vector2), typeof(StardewValley.TerrainFeatures.TerrainFeature))]
+    [HookRedirectConstructorFromBase("StardewValley.Farm", ".ctor", typeof(long), typeof(StardewValley.FarmAnimal))]
+    [HookRedirectConstructorFromBase("StardewValley.Farm", "System.Void StardewValley.Farm::.ctor(xTile.Map,System.String)", typeof(long), typeof(StardewValley.FarmAnimal))]
+    [HookRedirectConstructorFromBase("StardewValley.Farmer", ".ctor", typeof(string), typeof(int))]
+    //[HookRedirectConstructorFromBase("StardewValley.Farmer", "System.Void StardewValley.Farmer::.ctor(StardewValley.FarmerSprite,Microsoft.Xna.Framework.Vector2,System.Int32,System.String,System.Collections.Generic.List<StardewValley.Item>,System.Boolean)", typeof(string), typeof(int))]
+    [HookRedirectConstructorFromBase("StardewValley.Farmer", "caughtFish", typeof(int), typeof(int[]))]
+    [HookRedirectConstructorFromBase("StardewValley.Farmer", "cookedRecipe", typeof(int), typeof(int))]
+    [HookRedirectConstructorFromBase("StardewValley.Farmer", "foundArtifact", typeof(int), typeof(int[]))]
+    [HookRedirectConstructorFromBase("StardewValley.Farmer", "foundMineral", typeof(int), typeof(int))]
     [HookRedirectConstructorFromBase("StardewValley.GameLocation", "shiftObjects", typeof(Vector2), typeof(StardewValley.Object))]
     [HookRedirectConstructorFromBase("StardewValley.GameLocation", ".ctor", typeof(Vector2), typeof(StardewValley.Object))]
+    [HookRedirectConstructorFromBase("StardewValley.GameLocation", ".ctor", typeof(Vector2), typeof(StardewValley.TerrainFeatures.TerrainFeature))]
     [HookRedirectConstructorFromBase("StardewValley.GameLocation", "System.Void StardewValley.GameLocation::.ctor(xTile.Map,System.String)", typeof(Vector2), typeof(StardewValley.Object))]
+    [HookRedirectConstructorFromBase("StardewValley.GameLocation", "System.Void StardewValley.GameLocation::.ctor(xTile.Map,System.String)", typeof(Vector2), typeof(StardewValley.TerrainFeatures.TerrainFeature))]
+    [HookRedirectConstructorFromBase("StardewValley.Locations.CommunityCenter", "System.Void StardewValley.Locations.CommunityCenter::.ctor(System.String)", typeof(int), typeof(bool))]
+    [HookRedirectConstructorFromBase("StardewValley.Locations.CommunityCenter", "System.Void StardewValley.Locations.CommunityCenter::.ctor(System.String)", typeof(int), typeof(bool[]))]
+    [HookRedirectConstructorFromBase("StardewValley.Locations.FarmHouse", "shiftObjects", typeof(Vector2), typeof(StardewValley.TerrainFeatures.TerrainFeature))]
+    [HookRedirectConstructorFromBase("StardewValley.Locations.LibraryMuseum", "System.Void StardewValley.Locations.LibraryMuseum::.ctor(xTile.Map,System.String)", typeof(Vector2), typeof(int))]
+    [HookRedirectConstructorFromBase("StardewValley.Locations.MineShaft", ".ctor", typeof(int), typeof(StardewValley.Locations.MineInfo))]
+    [HookRedirectConstructorFromBase("StardewValley.Menus.CraftingPage", ".ctor", typeof(string), typeof(int))]
+    [HookRedirectConstructorFromBase("StardewValley.Stats", ".ctor", typeof(string), typeof(int))]
     public class FhSerializableDictionary<TKey, TValue> : SerializableDictionary<TKey, TValue>
     {
         public FhSerializableDictionary()
         {
-            Farmhand.Logging.Log.Error("Using overwritten serialized dictionary");
+            
         }
 
         public new void ReadXml(XmlReader reader)
