e02c6556cbb89e45a5a37a8bad2814448b5d54c3
ClxS <slxxls92@gmail.com>
2016-10-07 23:54:48 +0100

    Added warning on monster conflicts

--- Libraries/Farmhand/API/Monsters/Monster.cs
+++ Libraries/Farmhand/API/Monsters/Monster.cs
@@ -12,6 +12,11 @@ namespace Farmhand.API.Monsters
 
         public static void RegisterMonster(MonsterInformation monsterInformation)
         {
+            if (Monsters.ContainsKey(monsterInformation.Name) && Monsters[monsterInformation.Name] != monsterInformation)
+            {
+                Logging.Log.Warning($"Potential conflict registering new monster. Monster {monsterInformation.Name} has been registered by two separate mods." +
+                                    $"Only the last registered one will be used.");
+            }
             Monsters[monsterInformation.Name] = monsterInformation;
         }
     }
