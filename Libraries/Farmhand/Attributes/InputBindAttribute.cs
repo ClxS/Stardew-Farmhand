de292d7ae30dae997478d4f24bf2b4f3b643d724
TownEater <TownEater@gmail.com>
2016-10-17 12:02:33 -0600

    Dialogue Injection
    - Added functionality to the API to allow for the hooking and creation
    of dialogue windows. Dialogue questions can be added, removed, or have
    their functionality changed. Whole new dialogue windows can be created
    and promped to the player.
    
    - Added Dialogues folder to the Farmhand.API, where most of this new
    functionality takes place, particularly in Dialogue.cs
    
    - Changed Input Bind Attribute to allow passing the parameter as a ref
    if desired
    
    - Added an example mod that shows how to change the carpenter dialogue
    menu
    
    - There is still one bug, and it is that any dialogue answers that call
    BusStop.answerDialogue, Desert.answerDialogue, and
    JojaMart.answerDialogue will have their answer result delegates called
    twice, because these answerDialogue methods call the base
    GameLocation.answerDialog method, and both are being hooked. This can be
    prevented by setting the doDefault flag in the result delegate to false,
    preventing any further stuff happening because of an answer.

--- Libraries/Farmhand/Attributes/InputBindAttribute.cs
+++ Libraries/Farmhand/Attributes/InputBindAttribute.cs
@@ -5,13 +5,15 @@ namespace Farmhand.Attributes
     [AttributeUsage(AttributeTargets.Parameter)]
     public class InputBindAttribute : ParameterBindAttribute
     {
-        public InputBindAttribute(Type type, string name)
+        public InputBindAttribute(Type type, string name, bool @ref = false)
         {
             Type = type;
             Name = name;
+            Ref = @ref;
         }
 
         public Type Type { get; set; }
         public string Name { get; set; }
+        public bool Ref { get; set; } = false;
     }
 }
---
+++ Mods/TestDialoguesMod/TestDialoguesMod.cs
@@ -0,0 +1,35 @@
+﻿using Farmhand;
+using Farmhand.API.Dialogues;
+
+namespace TestDialoguesMod
+{
+    public class TestDialoguesMod : Mod
+    {
+        public static TestDialoguesMod Instance;
+
+        public override void Entry()
+        {
+            Instance = this;
+
+            // Create a result, dictating what will happen if our answer is picked
+            DialogueResultInformation newRobinResult = new DialogueResultInformation(Instance, new Dialogue.AnswerResult(RobinTeleport));
+            // Create an answer, an option that will show up as a response to a question
+            DialogueAnswerInformation newRobinAnswer = new DialogueAnswerInformation(Instance, "RobinTeleport", "Teleport", newRobinResult);
+            // Register our new answer to be included in robins's question
+            Dialogue.RegisterNewAnswer(Questions.Carpenter, newRobinAnswer);
+
+            // Remove robin's "Leave" option
+            DialogueAnswerRemovalInformation removeRobinLeave = new DialogueAnswerRemovalInformation(Instance, Answers.Carpenter_Leave);
+            Dialogue.RemoveDefaultAnswer(Questions.Carpenter, removeRobinLeave);
+        }
+
+        public static void RobinTeleport(ref bool doDefault)
+        {
+            StardewValley.Game1.player.Halt();
+            StardewValley.Game1.player.freezePause = 700;
+            StardewValley.Game1.warpFarmer("Town", 105, 80, 1);
+
+            doDefault = true;
+        }
+    }
+}
