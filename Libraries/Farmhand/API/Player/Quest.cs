cd71d6abed4750920b31f85c7b949c9c7c1b6b0d
Jonathan Fröst <Kithio.Lost@gmail.com>
2016-10-25 14:54:58 +0200

    Added Quest Injection Added Mail Injection Added Test Mods for Mail and Quest Injection (Goes hand-in-hand with eachother)

---
+++ Libraries/Farmhand/API/Player/Quest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace Farmhand.API.Player
+{
+    public class Quest
+    {
+        public static Dictionary<int, QuestInformation> Quests { get; } = new Dictionary<int, QuestInformation>();
+
+        public static void RegisterQuest(QuestInformation questInformation)
+        {
+            if (Quests.ContainsKey(questInformation.Id) && Quests[questInformation.Id] != questInformation)
+            {
+                Logging.Log.Warning($"Potential conflict registering new quest. Quest {questInformation.Id} has been registered by two separate mods." +
+                                    "Only the last registered one will be used.");
+            }
+            Quests[questInformation.Id] = questInformation;
+        }
+    }
+}
---
+++ Libraries/Farmhand/API/Player/QuestInformation.cs
@@ -0,0 +1,134 @@
+﻿using System;
+
+namespace Farmhand.API.Player
+{
+    public class QuestInformation
+    {
+        public int Id { get; set; }
+
+        public enum QuestType
+        {
+            Building,
+            Crafting,
+            ItemHarvest,
+            ItemDelivery,
+            Location,
+            LostItem,
+            Monster
+        }
+
+        public QuestType Type { get; set; }
+
+        public string QuestTitle { get; set; }
+        public string QuestDescription { get; set; }
+
+        public Objective QuestObjective { get; set; }
+        
+        public class Objective
+        {
+            public string ObjectiveDescription { get; set; }
+        }
+
+        public class BuildingObjective : Objective
+        {
+            public string BuildingName { get; set; }
+        }
+
+        public class CraftingObjective : Objective
+        {
+            public int ItemId { get; set; }
+            public bool BigCraftable { get; set; }
+        }
+
+        public class ItemDeliveryObjective : Objective
+        {
+            public string NpcName { get; set; }
+            public int ItemId { get; set; }
+            public int Amount { get; set; } = 1;
+
+            public string CompletionMessage { get; set; }
+        }
+
+        public class ItemHarvestObjective : Objective
+        {
+            public int ItemId { get; set; }
+            public int Amount { get; set; } = 1;
+        }
+
+        public class LocationObjective : Objective
+        {
+            public string MapName { get; set; }
+        }
+
+        public class LostItemObjective : Objective
+        {
+            public string NpcName { get; set; }
+            public int ItemId { get; set; }
+            public string MapName { get; set; }
+            public int ItemXPos { get; set; }
+            public int ItemYPos { get; set; }
+
+            public string CompletionMessage { get; set; }
+        }
+
+        public class MonsterObjective : Objective
+        {
+            public string MonsterName { get; set; }
+            public string Amount { get; set; }
+        }
+
+        public int[] NextQuests { get; set; } = { -1 };
+
+        public int MoneyReward { get; set; } = -1;
+        public string RewardDescription { get; set; } = "-1";
+
+        public bool CanBeCancelled { get; set; }
+
+        public string GetObjective()
+        {
+            switch (Type)
+            {
+                case QuestType.Building:
+                    var buildObj = (BuildingObjective)QuestObjective;
+                    return $"{buildObj.BuildingName}";
+                case QuestType.Crafting:
+                    var craftObj = (CraftingObjective)QuestObjective;
+                    return $"{craftObj.ItemId} {craftObj.BigCraftable}";
+                case QuestType.ItemHarvest:
+                    var harvObj = (ItemHarvestObjective)QuestObjective;
+                    return $"{harvObj.ItemId} {harvObj.Amount}";
+                case QuestType.ItemDelivery:
+                    var delObj = (ItemDeliveryObjective)QuestObjective;
+                    return $"{delObj.NpcName} {delObj.ItemId} {delObj.Amount}";
+                case QuestType.Location:
+                    var locObj = (LocationObjective)QuestObjective;
+                    return $"{locObj.MapName}";
+                case QuestType.LostItem:
+                    var lostObj = (LostItemObjective)QuestObjective;
+                    return $"{lostObj.NpcName} {lostObj.ItemId} {lostObj.MapName} {lostObj.ItemXPos} {lostObj.ItemYPos}";
+                case QuestType.Monster:
+                    var monsterObj = (MonsterObjective)QuestObjective;
+                    return $"{monsterObj.MonsterName} {monsterObj.Amount}";
+                default:
+                    throw new Exception($"Invalid QuestType {Type}");
+            }
+        }
+
+        public override string ToString()
+        {
+            switch (Type)
+            {
+                case QuestType.ItemDelivery:
+                case QuestType.LostItem:
+                    var objective = QuestObjective as LostItemObjective;
+                    var msg = objective != null
+                        ? objective.CompletionMessage
+                        : ((ItemDeliveryObjective) QuestObjective).CompletionMessage;
+
+                    return $"{Type}/{QuestTitle}/{QuestDescription}/{QuestObjective.ObjectiveDescription}/{GetObjective()}/{string.Join(" ", NextQuests)}/{MoneyReward}/{RewardDescription}/{CanBeCancelled}/{msg}";
+            }
+            
+            return $"{Type}/{QuestTitle}/{QuestDescription}/{QuestObjective.ObjectiveDescription}/{GetObjective()}/{string.Join(" ", NextQuests)}/{MoneyReward}/{RewardDescription}/{CanBeCancelled}";
+        }
+    }
+}
