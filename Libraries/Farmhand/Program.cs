702958b6d3a5b616bdad38fd9d4a5f8d77d4baa2
Jonathan Fröst <Kithio.Lost@gmail.com>
2016-11-01 02:26:21 +0100

    Added FarmhandConfig + Debug Mode Option
    Only contains bool for "Debug Mode" right now.

---
+++ Libraries/Farmhand/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using Newtonsoft.Json;
+
+namespace Farmhand
+{
+    public class Program
+    {
+        public static FarmhandConfig Config { get; set; }
+        
+        public static void ReadConfig()
+        {
+            try
+            {
+                var cfg = File.ReadAllText("FarmhandConfig.json");
+                Config = JsonConvert.DeserializeObject<FarmhandConfig>(cfg);
+            }
+            catch (FileNotFoundException)
+            {
+                Config = new FarmhandConfig();
+                var cfg = JsonConvert.SerializeObject(Config, Formatting.Indented);
+                File.WriteAllText("FarmhandConfig.json", cfg);
+            }
+        }
+    }
+}
