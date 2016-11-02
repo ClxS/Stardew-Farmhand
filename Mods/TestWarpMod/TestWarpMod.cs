7812cb50dfdc77adc6d404cbe94e9bcf2771d638
spacechase0 <spacechase0@users.noreply.github.com>
2016-11-02 15:59:14 -0400

    Added test mod for OnBeforeWarp.

---
+++ Mods/TestWarpMod/TestWarpMod.cs
@@ -0,0 +1,26 @@
+﻿using Farmhand;
+using Farmhand.Events.Arguments.LocationEvents;
+
+namespace TestWarpMod
+{
+    public class TestWarpMod : Mod
+    {
+        public static TestWarpMod Instance;
+
+        public override void Entry()
+        {
+            Instance = this;
+
+            Farmhand.Events.LocationEvents.OnBeforeWarp += LinusPrivacy;
+        }
+
+        public static void LinusPrivacy( object sender, EventArgsOnBeforeWarp args )
+        {
+            if ( args.LocationAfterWarp.Name == "Tent" && ( StardewValley.Game1.timeOfDay < 900 || StardewValley.Game1.timeOfDay >= 2000 ) )
+            {
+                args.Cancel = true;
+                StardewValley.Game1.drawObjectDialogue("Linus is probably asleep right now.");
+            }
+        }
+    }
+}
