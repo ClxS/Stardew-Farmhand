ce8b70644b48479a515933892297e4f5ba3c84c3
ClxS <slxxls92@gmail.com>
2016-12-21 07:19:02 +0000

    Documented and styled UI Interfaces

--- Libraries/FarmhandUI/Interfaces/IIinteractiveMenuComponent.cs
+++ Libraries/FarmhandUI/Interfaces/IIinteractiveMenuComponent.cs
@@ -1,19 +1,125 @@
-﻿using Microsoft.Xna.Framework;
-
-namespace Farmhand.UI.Interfaces
+﻿namespace Farmhand.UI.Interfaces
 {
+    using Microsoft.Xna.Framework;
+
+    /// <summary>
+    ///     The InteractiveMenuComponent interface.
+    /// </summary>
     public interface IInteractiveMenuComponent : IMenuComponent
     {
+        /// <summary>
+        ///     Checks whether a point is within the event area of this component.
+        /// </summary>
+        /// <param name="position">
+        ///     The position to check.
+        /// </param>
+        /// <param name="offset">
+        ///     The offset of this component.
+        /// </param>
+        /// <returns>
+        ///     Whether the point is within the component bounds.
+        /// </returns>
         bool InBounds(Point position, Point offset);
+
+        /// <summary>
+        ///     Called when the left mouse button is clicked over this component
+        /// </summary>
+        /// <param name="position">
+        ///     The mouse position
+        /// </param>
+        /// <param name="offset">
+        ///     The offset of this component
+        /// </param>
         void LeftClick(Point position, Point offset);
+
+        /// <summary>
+        ///     Called when the right mouse button is clicked over this component
+        /// </summary>
+        /// <param name="position">
+        ///     The mouse position
+        /// </param>
+        /// <param name="offset">
+        ///     The offset of this component
+        /// </param>
         void RightClick(Point position, Point offset);
+
+        /// <summary>
+        ///     Called when the left mouse button is held over this component
+        /// </summary>
+        /// <param name="position">
+        ///     The mouse position
+        /// </param>
+        /// <param name="offset">
+        ///     The offset of this component
+        /// </param>
         void LeftHeld(Point position, Point offset);
+
+        /// <summary>
+        ///     Called when the left mouse button is released over this component
+        /// </summary>
+        /// <param name="position">
+        ///     The mouse position
+        /// </param>
+        /// <param name="offset">
+        ///     The offset of this component
+        /// </param>
         void LeftUp(Point position, Point offset);
+
+        /// <summary>
+        ///     Called when the mouse enters this component
+        /// </summary>
+        /// <param name="position">
+        ///     The mouse position
+        /// </param>
+        /// <param name="offset">
+        ///     The offset of this component
+        /// </param>
         void HoverIn(Point position, Point offset);
+
+        /// <summary>
+        ///     Called when the mouse leaves this component
+        /// </summary>
+        /// <param name="position">
+        ///     The mouse position
+        /// </param>
+        /// <param name="offset">
+        ///     The offset of this component
+        /// </param>
         void HoverOut(Point position, Point offset);
+
+        /// <summary>
+        ///     Called when the mouse moves over this component
+        /// </summary>
+        /// <param name="position">
+        ///     The mouse position
+        /// </param>
+        /// <param name="offset">
+        ///     The offset of this component
+        /// </param>
         void HoverOver(Point position, Point offset);
+
+        /// <summary>
+        ///     Called when focus is lost
+        /// </summary>
         void FocusLost();
+
+        /// <summary>
+        ///     Called when focus is gained
+        /// </summary>
         void FocusGained();
+
+        /// <summary>
+        ///     Called when the scroll wheel is used over this component
+        /// </summary>
+        /// <param name="direction">
+        ///     The delta value of the scroll
+        /// </param>
+        /// <param name="position">
+        ///     The mouse position
+        /// </param>
+        /// <param name="offset">
+        ///     The offset of this component
+        /// </param>
         void Scroll(int direction, Point position, Point offset);
     }
 }
--- Libraries/FarmhandUI/Interfaces/IKeyboardComponent.cs
+++ Libraries/FarmhandUI/Interfaces/IKeyboardComponent.cs
@@ -1,11 +1,47 @@
 ﻿namespace Farmhand.UI.Interfaces
 {
+    using Microsoft.Xna.Framework.Input;
+
+    /// <summary>
+    ///     The KeyboardComponent interface.
+    /// </summary>
     public interface IKeyboardComponent
     {
+        /// <summary>
+        ///     Gets or sets a value indicating whether selected.
+        /// </summary>
         bool Selected { get; set; }
+
+        /// <summary>
+        ///     Called when a character is entered
+        /// </summary>
+        /// <param name="chr">
+        ///     The character received
+        /// </param>
         void TextReceived(char chr);
+
+        /// <summary>
+        ///     Called when a string is entered
+        /// </summary>
+        /// <param name="str">
+        ///     The string received
+        /// </param>
         void TextReceived(string str);
+
+        /// <summary>
+        ///     Called when a command is received
+        /// </summary>
+        /// <param name="cmd">
+        ///     The command received
+        /// </param>
         void CommandReceived(char cmd);
-        void SpecialReceived(Microsoft.Xna.Framework.Input.Keys key);
+
+        /// <summary>
+        ///     Called when a special key is received
+        /// </summary>
+        /// <param name="key">
+        ///     The key received
+        /// </param>
+        void SpecialReceived(Keys key);
     }
 }
