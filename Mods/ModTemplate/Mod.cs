7fb111e844cf933a6d6d7820aa2ecfbfa3fd943d
ClxS <slxxls92@gmail.com>
2016-12-28 16:22:53 +0000

    Added a template mod project for use with deploying the installer

---
+++ Mods/ModTemplate/Mod.cs
@@ -0,0 +1,18 @@
+﻿namespace ModTemplate
+{
+    using Farmhand;
+
+    internal class Mod : Farmhand.Mod
+    {
+        #region ModSettings
+        private readonly Settings settings = new Settings();
+
+        public override ModSettings ConfigurationSettings => this.settings;
+        #endregion
+
+        public override void Entry()
+        {
+            // Add your entry logic here
+        }
+    }
+}
