e267b0fdaefef62b80d65e550f5dc6d170d5a053
ClxS <slxxls92@gmail.com>
2016-12-29 11:34:17 +0000

    ModTemplateUtility now properly removes the Settings file if the option is disabled

--- Installers/FarmhandInstaller.UI/Utilities/ModTemplateUtility.cs
+++ Installers/FarmhandInstaller.UI/Utilities/ModTemplateUtility.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using System.IO;
+    using System.Text.RegularExpressions;
 
     internal static class ModTemplateUtility
     {
@@ -23,16 +24,30 @@
                 }
 
                 var fileContents = File.ReadAllText(filePath);
-                fileContents = fileContents.Replace("ModTemplate", InstallationContext.ModSettings.Name);
+                fileContents = fileContents.Replace("ModTemplateWS", InstallationContext.ModSettings.Name);
+                fileContents = fileContents.Replace("ModTemplate", InstallationContext.ModSettings.NameNoSpace);
                 fileContents = fileContents.Replace("ModAuthor", InstallationContext.ModSettings.Author);
                 fileContents = fileContents.Replace("ModDescription", InstallationContext.ModSettings.Description);
                 File.WriteAllText(filePath, fileContents);
             }
 
+            if (!InstallationContext.ModSettings.UsesSettingsFile)
+            {
+                File.Delete(Path.Combine(newModRoot, "Settings.cs"));
+                var projFile = Path.Combine(newModRoot, "ModTemplate.csproj");
+                var text = File.ReadAllText(projFile);
+                text = Regex.Replace(text, ".*Settings\\.cs.*", string.Empty);
+                File.WriteAllText(projFile, text);
+
+                var mainFile = Path.Combine(newModRoot, "Mod.cs");
+                text = File.ReadAllText(mainFile);
+                text = Regex.Replace(text, @".*#region(.|\n)*#endregion\s\n\s\s", string.Empty);
+                File.WriteAllText(mainFile, text);
+            }
+            
             File.Move(
                 Path.Combine(newModRoot, "ModTemplate.csproj"),
                 Path.Combine(newModRoot, InstallationContext.ModSettings.NameNoSpace + ".csproj"));
-
         }
     }
 }
