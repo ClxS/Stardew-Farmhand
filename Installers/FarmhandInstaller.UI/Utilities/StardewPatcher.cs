9cdd8a9cd83a6ead44bbf2032197540e7bc97571
ClxS <slxxls92@gmail.com>
2016-12-31 11:34:53 +0000

    Patcher is now loaded into a separate AppDomain and unloaded after patching
    This is so loading our temp assemblies doesn't lock the directory, letting us clean up after ourselves.

--- Installers/FarmhandInstaller.UI/Utilities/StardewPatcher.cs
+++ Installers/FarmhandInstaller.UI/Utilities/StardewPatcher.cs
@@ -8,34 +8,69 @@
 
     internal static class StardewPatcher
     {
-        private static Patcher CreatePatcher(StardewPatcherPass pass, string assemblyLocation)
+        private class PatcherProxy : MarshalByRefObject
         {
-            var patcherAssembly =
-                Assembly.LoadFrom(
-                    pass == StardewPatcherPass.PassOne
-                        ? Path.Combine(assemblyLocation, "FarmhandPatcherFirstPass.dll")
-                        : Path.Combine(assemblyLocation, "FarmhandPatcherSecondPass.dll"));
-            var patcherType = patcherAssembly.GetType(
-                    pass == StardewPatcherPass.PassOne 
-                        ? "Farmhand.PatcherFirstPass" 
-                        : "Farmhand.PatcherSecondPass");
-            var patcher = Activator.CreateInstance(patcherType);
-            return patcher as Patcher;
+            private Patcher Patcher { get; set; }
+
+            public void Initialize(string assemblyPath, string type)
+            {
+                var test = Assembly.UnsafeLoadFrom(assemblyPath);
+                var patcherType = test.GetType(type);
+                this.Patcher = (Patcher)Activator.CreateInstance(patcherType);
+            }
+
+            public void LoadCommon(string assemblyDirectory)
+            {
+                Assembly.UnsafeLoadFrom(Path.Combine(assemblyDirectory, "FarmhandPatcherCommon.dll"));
+                Assembly.UnsafeLoadFrom(Path.Combine(assemblyDirectory, "Farmhand.dll"));
+            }
+
+            public void SetOptions(string assemblyDirectory, bool disableGrm = true)
+            {
+                this.Patcher.Options.DisableGrm = disableGrm;
+                this.Patcher.Options.AssemblyDirectory = assemblyDirectory;
+            }
+
+            public void Patch(string path)
+            {
+                this.Patcher.PatchStardew(path);
+            }
+        }
+
+        private static PatcherProxy CreatePatcher(StardewPatcherPass pass, string assemblyLocation, AppDomain domain)
+        {
+            Type type = typeof(PatcherProxy);
+            var proxy = (PatcherProxy)domain.CreateInstanceAndUnwrap(
+                type.Assembly.FullName,
+                type.FullName);
+                
+            proxy.Initialize(
+                pass == StardewPatcherPass.PassOne
+                    ? Path.Combine(assemblyLocation, "FarmhandPatcherFirstPass.dll")
+                    : Path.Combine(assemblyLocation, "FarmhandPatcherSecondPass.dll"),
+                pass == StardewPatcherPass.PassOne ? "Farmhand.PatcherFirstPass" : "Farmhand.PatcherSecondPass");
+            return proxy;
         }
 
         public static void Patch(string outputPath, string assemblyDirectory, bool disableGrm = true)
         {
+            var domaininfo = new AppDomainSetup { ApplicationBase = Environment.CurrentDirectory };
+            var adevidence = AppDomain.CurrentDomain.Evidence;
+            var localDomain = AppDomain.CreateDomain("LocalDomain", adevidence, domaininfo);
+
             var sdvPath = Path.Combine(InstallationContext.StardewPath, "Stardew Valley.exe");
             
-            var patcher = CreatePatcher(StardewPatcherPass.PassOne, assemblyDirectory);
-            patcher.Options.AssemblyDirectory = assemblyDirectory;
-            patcher.Options.DisableGrm = disableGrm;
-            patcher.PatchStardew(sdvPath);
+            var patcher = CreatePatcher(StardewPatcherPass.PassOne, assemblyDirectory, localDomain);
+            patcher.LoadCommon(assemblyDirectory);
+            patcher.SetOptions(assemblyDirectory, disableGrm);
+            patcher.Patch(sdvPath);
 
-            patcher = CreatePatcher(StardewPatcherPass.PassTwo, assemblyDirectory);
-            patcher.Options.AssemblyDirectory = assemblyDirectory;
-            patcher.Options.DisableGrm = disableGrm;
-            patcher.PatchStardew(outputPath);
+            patcher = CreatePatcher(StardewPatcherPass.PassTwo, assemblyDirectory, localDomain);
+            patcher.LoadCommon(assemblyDirectory);
+            patcher.SetOptions(assemblyDirectory, disableGrm);
+            patcher.Patch(outputPath);
+
+            AppDomain.Unload(localDomain);
         }
     }
 }
