38016e14b0d71c245337741a6d063b85f8a2e270
ClxS <slxxls92@gmail.com>
2016-12-31 12:19:49 +0000

    Added Player package handler

--- Installers/FarmhandInstaller.UI/Utilities/Package/PlayerPackage.cs
+++ Installers/FarmhandInstaller.UI/Utilities/Package/PlayerPackage.cs
@@ -1,14 +1,32 @@
 ﻿namespace FarmhandInstaller.UI.Utilities.Package
 {
     using System;
+    using System.IO;
 
     internal class PlayerPackage : IPackage
     {
+        public const string PackageFile = "FarmhandInstaller.UI.Payload.Player.package";
+
         #region IPackage Members
 
         public void Install()
         {
-            throw new NotImplementedException();
+            var farmhandBinary = Path.Combine(InstallationContext.OutputPath, "Stardew Farmhand.exe");
+            var temporaryDirectory = Path.Combine(InstallationContext.OutputPath, "FarmhandInstallTemp");
+            var workingDirectory = Path.Combine(temporaryDirectory, "WorkingDirectory");
+            var binDirectory = Path.Combine(temporaryDirectory, "Bin");
+
+            DirectoryUtility.EnsureDirectoryExists(InstallationContext.OutputPath);
+            DirectoryUtility.CleanDirectory(InstallationContext.OutputPath);
+            DirectoryUtility.EnsureDirectoryExists(temporaryDirectory);
+            PackageManager.ExtractPackageFile(PackageFile, temporaryDirectory);
+            
+            DirectoryUtility.CopyAll(InstallationContext.StardewPath, InstallationContext.OutputPath, ".*\\.exe");
+            DirectoryUtility.CopyAll(workingDirectory, InstallationContext.OutputPath);
+
+            StardewPatcher.Patch(farmhandBinary, binDirectory);
+
+            Directory.Delete(temporaryDirectory, true);
         }
 
         #endregion
