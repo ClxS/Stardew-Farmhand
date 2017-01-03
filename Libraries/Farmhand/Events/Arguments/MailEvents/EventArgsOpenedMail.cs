ce3d7a9c92a4b0ea6dc582af0daa7b593f897f2f
ClxS <slxxls92@gmail.com>
2017-01-03 20:56:06 +0000

    Added Mail events

---
+++ Libraries/Farmhand/Events/Arguments/MailEvents/EventArgsOpenedMail.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Farmhand.Events.Arguments.MailEvents
+{
+    public class EventArgsOpenedMail : EventArgs
+    {
+        public EventArgsOpenedMail(string message)
+        {
+            this.Message = message;
+        }
+
+        public string Message { get; set; }
+    }
+}
---
+++ Libraries/Farmhand/Events/MailEvents.cs
@@ -0,0 +1,34 @@
+﻿namespace Farmhand.Events
+{
+    using System;
+
+    using Farmhand.Attributes;
+    using Farmhand.Events.Arguments.MailEvents;
+
+    public class MailEvents
+    {
+        public static event EventHandler<EventArgsOpenedMail> OpenedMail = delegate { };
+
+        public static event EventHandler<EventArgsOpenedMailWithAttachment> OpenedMailWithAttachment = delegate { };
+
+        [Hook(HookType.Entry, "StardewValley.Menus.LetterViewerMenu",
+            "System.Void StardewValley.Menus.LetterViewerMenu::.ctor(System.String)")]
+        internal static void OnOpenedMail([ThisBind] object @this, [InputBind(typeof(string), "text")] string text)
+        {
+            EventCommon.SafeInvoke(OpenedMail, @this, new EventArgsOpenedMail(text));
+        }
+
+        [Hook(HookType.Entry, "StardewValley.Menus.LetterViewerMenu",
+            "System.Void StardewValley.Menus.LetterViewerMenu::.ctor(System.String,System.String)")]
+        internal static void OnOpenedMailWithAttachment(
+            [ThisBind] object @this,
+            [InputBind(typeof(string), "mail")] string mail,
+            [InputBind(typeof(string), "mailTitle")] string mailTitle)
+        {
+            EventCommon.SafeInvoke(
+                OpenedMailWithAttachment,
+                @this,
+                new EventArgsOpenedMailWithAttachment(mail, mailTitle));
+        }
+    }
+}
