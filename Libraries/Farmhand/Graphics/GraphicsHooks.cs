574793900a8d59214f2eda2fce664d5eaa8b8466
ClxS <slxxls92@gmail.com>
2017-01-21 23:26:38 +0000

    Moved the graphics hooks into their own class

---
+++ Libraries/Farmhand/Graphics/GraphicsHooks.cs
@@ -0,0 +1,30 @@
+﻿namespace Farmhand.Graphics
+{
+    using Farmhand.Attributes;
+
+    using Microsoft.Xna.Framework.Graphics;
+
+    using StardewValley;
+
+    internal static class GraphicsHooks
+    {
+        [Hook(HookType.Exit, "StardewValley.Game1", ".ctor")]
+        internal static void SetHiDefProfile()
+        {
+            Game1.graphics.GraphicsProfile = GraphicsProfile.HiDef;
+            Game1.graphics.PreparingDeviceSettings +=
+                (sender, args) =>
+                    {
+                        args.GraphicsDeviceInformation.PresentationParameters.RenderTargetUsage =
+                            RenderTargetUsage.PreserveContents;
+                    };
+        }
+
+        [Hook(HookType.Exit, "StardewValley.Game1", "Window_ClientSizeChanged")]
+        [Hook(HookType.Exit, "StardewValley.Game1", "Initialize")]
+        internal static void EnforcePreserveContents()
+        {
+
+        }
+    }
+}
