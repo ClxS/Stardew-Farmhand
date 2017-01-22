0da9af66ed4809b3ea76a8d3993840f31669bdce
ClxS <slxxls92@gmail.com>
2017-01-22 16:58:38 +0000

    Added NPC before checkaction

---
+++ Libraries/Farmhand/Events/Arguments/NpcEvents/BeforeCheckAction.cs
@@ -0,0 +1,61 @@
+﻿namespace Farmhand.Events.Arguments.NpcEvents
+{
+    using Farmhand.Events.Arguments.Common;
+
+    using StardewValley;
+
+    /// <summary>
+    ///     Arguments for BeforeCheckAction.
+    /// </summary>
+    public class BeforeCheckActionEventArgs : ReturnableEventArgs
+    {
+        private bool handled;
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="BeforeCheckActionEventArgs" /> class.
+        /// </summary>
+        /// <param name="gameLocation">
+        ///     The game location.
+        /// </param>
+        /// <param name="farmer">
+        ///     The farmer.
+        /// </param>
+        public BeforeCheckActionEventArgs(GameLocation gameLocation, Farmer farmer)
+        {
+            this.GameLocation = gameLocation;
+            this.Farmer = farmer;
+            this.handled = false;
+        }
+
+        /// <summary>
+        ///     Gets the location where this event is
+        /// </summary>
+        public GameLocation GameLocation { get; }
+
+        /// <summary>
+        ///     Gets the farmer who triggered this event
+        /// </summary>
+        public Farmer Farmer { get; }
+
+        /// <summary>
+        ///     Gets or sets a value indicating whether this event has been handled by a mod.
+        /// </summary>
+        /// <remarks>
+        ///     Note that if another mod has handled this action,
+        ///     setting this to false will not prevent that mod marking it as handled.
+        /// </remarks>
+        public bool Handled
+        {
+            get
+            {
+                return this.handled;
+            }
+
+            set
+            {
+                this.handled = value;
+                this.IsHandled = true;
+            }
+        }
+    }
+}
---
+++ Libraries/Farmhand/Events/NpcEvents.cs
@@ -0,0 +1,32 @@
+﻿namespace Farmhand.Events
+{
+    using System;
+
+    using Farmhand.Attributes;
+    using Farmhand.Events.Arguments.NpcEvents;
+
+    using StardewValley;
+
+    /// <summary>
+    ///     Contains events related to mailing.
+    /// </summary>
+    public static class NpcEvents
+    {
+        /// <summary>
+        ///     Fires upon exiting an in-game event.
+        /// </summary>
+        public static event EventHandler<BeforeCheckActionEventArgs> BeforeCheckAction = delegate { };
+
+        [HookReturnable(HookType.Entry, "StardewValley.NPC", "checkAction")]
+        internal static bool OnBeforeLocationLoadObjects(
+            [UseOutputBind] ref bool useOutput,
+            [ThisBind] object @this,
+            [InputBind(typeof(Farmer), "who")] Farmer who,
+            [InputBind(typeof(GameLocation), "l")] GameLocation location)
+        {
+            var eventArgs = new BeforeCheckActionEventArgs(location, who);
+            EventCommon.SafeInvoke(BeforeCheckAction, @this, eventArgs);
+            return eventArgs.Handled;
+        }
+    }
+}
