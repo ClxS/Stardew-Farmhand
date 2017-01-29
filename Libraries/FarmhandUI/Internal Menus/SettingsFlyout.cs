18d6cfa5b50cf8e77ef2b96d17a1139af1571034
ClxS <slxxls92@gmail.com>
2017-01-29 21:18:35 +0000

    Added Settings flyout handler
    It is currently empty. Committing this so I can access it elsewhere.

---
+++ Libraries/FarmhandUI/Internal Menus/SettingsFlyout.cs
@@ -0,0 +1,34 @@
+﻿namespace Farmhand.UI.Internal_Menus
+{
+    using System;
+
+    using Farmhand.Attributes;
+    using Farmhand.Events;
+    using Farmhand.Events.Arguments.GraphicsEvents;
+
+    using StardewValley;
+
+    internal static class SettingsFlyoutHandler
+    {
+        private static bool isFlyoutVisible = false;
+
+        private static bool isFlyoutSettingsWindowOpen = false;
+
+        [Hook(HookType.Entry, "StardewValley.Game1", ".ctor")]
+        internal static void AttachListeners()
+        {
+            GraphicsEvents.PostRenderGuiEventNoCheck += GraphicsEvents_PostRenderGuiEventNoCheck;
+        }
+
+        private static void GraphicsEvents_PostRenderGuiEventNoCheck(object sender, DrawEventArgs e)
+        {
+            if (isFlyoutVisible && isFlyoutSettingsWindowOpen)
+            {
+                return;
+            }
+
+            var mouseX = Game1.getMouseX();
+            var mouseY = Game1.getMouseY();
+        }
+    }
+}
