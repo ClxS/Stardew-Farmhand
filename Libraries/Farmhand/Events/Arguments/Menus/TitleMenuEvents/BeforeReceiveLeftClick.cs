4aed51d10fd266a69defac70e7fae7feaeef8c0e
ClxS <slxxls92@gmail.com>
2017-01-30 21:04:06 +0000

    Added left click titlemenu events

---
+++ Libraries/Farmhand/Events/Arguments/Menus/TitleMenuEvents/BeforeReceiveLeftClick.cs
@@ -0,0 +1,44 @@
+﻿namespace Farmhand.Events.Arguments.Menus.TitleMenuEvents
+{
+    using System.ComponentModel;
+
+    /// <summary>
+    ///     The after receive left click.
+    /// </summary>
+    public class BeforeReceiveLeftClick : CancelEventArgs
+    {
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="BeforeReceiveLeftClick" /> class.
+        /// </summary>
+        /// <param name="x">
+        ///     The x.
+        /// </param>
+        /// <param name="y">
+        ///     The y.
+        /// </param>
+        /// <param name="playSound">
+        ///     The play sound.
+        /// </param>
+        public BeforeReceiveLeftClick(int x, int y, bool playSound)
+        {
+            this.X = x;
+            this.Y = y;
+            this.PlaySound = playSound;
+        }
+
+        /// <summary>
+        ///     Gets the X position.
+        /// </summary>
+        public int X { get; }
+
+        /// <summary>
+        ///     Gets the Y position.
+        /// </summary>
+        public int Y { get; }
+
+        /// <summary>
+        ///     Gets a value indicating whether to play click sound.
+        /// </summary>
+        public bool PlaySound { get; }
+    }
+}
---
+++ Libraries/Farmhand/Events/Menus/TitleMenuEvents.cs
@@ -0,0 +1,52 @@
+﻿namespace Farmhand.Events.Menus
+{
+    using System;
+
+    using Farmhand.Attributes;
+    using Farmhand.Events.Arguments.Menus.TitleMenuEvents;
+
+    /// <summary>
+    ///     Contains events relating to the TitleMenu.
+    /// </summary>
+    public static class TitleMenuEvents
+    {
+        /// <summary>
+        ///     Fires just before the menu is clicked.
+        /// </summary>
+        /// <remarks>
+        ///     This is cancellable, allowing you to prevent clicks being passed to the title menu controls.
+        /// </remarks>
+        public static event EventHandler<BeforeReceiveLeftClick> BeforeReceiveLeftClick =
+            delegate { };
+
+        /// <summary>
+        ///     Fires just after the menu is clicked.
+        /// </summary>
+        public static event EventHandler<AfterReceiveLeftClick> AfterReceiveLeftClick = delegate { };
+
+        [Hook(HookType.Entry, "StardewValley.Menus.TitleMenu", "receiveLeftClick")]
+        internal static bool OnBeforeReceiveLeftClick(
+            [ThisBind] object @this,
+            [InputBind(typeof(int), "x")] int x,
+            [InputBind(typeof(int), "y")] int y,
+            [InputBind(typeof(bool), "playSound")] bool playSound)
+        {
+            var eventArgs = new BeforeReceiveLeftClick(x, y, playSound);
+            EventCommon.SafeInvoke(BeforeReceiveLeftClick, @this, eventArgs);
+            return eventArgs.Cancel;
+        }
+
+        [Hook(HookType.Exit, "StardewValley.Menus.TitleMenu", "receiveLeftClick")]
+        internal static void OnAfterReceiveLeftClick(
+            [ThisBind] object @this,
+            [InputBind(typeof(int), "x")] int x,
+            [InputBind(typeof(int), "y")] int y,
+            [InputBind(typeof(bool), "playSound")] bool playSound)
+        {
+            EventCommon.SafeInvoke(
+                AfterReceiveLeftClick,
+                @this,
+                new AfterReceiveLeftClick(x, y, playSound));
+        }
+    }
+}
