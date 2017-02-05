94921528764c8d6cab288ad3e7ca1f01f0bc4b37
ClxS <slxxls92@gmail.com>
2017-02-05 18:59:23 +0000

    Cleaned up the FarmhandConfig menu

--- Mods/ModTemplate/Pages/FarmhandConfig.cs
+++ Mods/ModTemplate/Pages/FarmhandConfig.cs
@@ -11,7 +11,8 @@
     using Farmhand.UI.Interfaces;
 
     using Microsoft.Xna.Framework;
-    using Microsoft.Xna.Framework.Graphics;
+
+    using ModTemplate.Controls;
 
     using StardewValley;
 
@@ -19,57 +20,33 @@
 
     internal sealed class FarmhandConfig : FrameworkMenu
     {
-        private readonly CheckboxFormComponent cachePortsBox;
+        private ColoredRectangleComponent background;
 
-        private readonly FrameworkMenu controlContainer;
+        private CheckboxFormComponent cachePortsBox;
 
-        private readonly CheckboxFormComponent debugModeBox;
+        private CheckboxFormComponent debugModeBox;
 
-        private readonly List<TextComponent> warningTextComponents;
+        private List<TextComponent> warningTextComponents;
 
         public FarmhandConfig()
             : base(FrameDimensions, false, false)
         {
-            this.controlContainer = new FrameworkMenu(new Point(100, 120), false);
-            var tab =
-                new FormCollectionComponent(
-                    new Rectangle(0, 0, this.ZoomEventRegion.Width, this.ZoomEventRegion.Height));
+            this.Centered = true;
+            var pos = Utility.getTopLeftPositionForCenteringOnScreen(100, 120);
+            var controlArea = new Rectangle(
+                (int)pos.X / Game1.pixelZoom - 50,
+                (int)pos.Y / Game1.pixelZoom - 60,
+                100,
+                120);
 
-            this.warningTextComponents = new List<TextComponent>
-            {
-                new TextComponent(
-                    new Point(0, 52),
-                    "You might need to restart"),
-                new TextComponent(
-                    new Point(0, 60),
-                    "the game for some changes"),
-                new TextComponent(
-                    new Point(0, 68),
-                    "to take affect")
-            };
+            var tab = new FormCollectionComponent(controlArea);
 
-            tab.AddComponent(new TextComponent(new Point(10, 0), "Farmhand Settings"));
-            this.debugModeBox = new CheckboxFormComponent(new Point(0, 12), "Debug Mode");
-            this.cachePortsBox = new CheckboxFormComponent(new Point(0, 24), "Cache Ports");
-            this.debugModeBox.Handler += this.FieldChanged_Handler;
-            this.cachePortsBox.Handler += this.FieldChanged_Handler;
+            this.AddControlBackgroundRect(tab);
+            this.AddControlTextLabels(tab);
+            this.AddControlOptions(tab);
+            this.AddControlButtons(tab);
 
-            tab.AddComponent(this.debugModeBox);
-            tab.AddComponent(this.cachePortsBox);
-
-            var saveButton = new ButtonFormComponent(new Point(4, 92), 70, "Save");
-            saveButton.Handler += this.SaveButton_Handler;
-            var cancelButton = new ButtonFormComponent(new Point(4, 80), 70, "Cancel");
-            cancelButton.Handler += this.CancelButton_Handler;
-
-            tab.AddComponent(saveButton);
-            tab.AddComponent(cancelButton);
-            foreach (var component in this.warningTextComponents)
-            {
-                tab.AddComponent(component);
-            }
-
-            this.controlContainer.AddComponent(tab);
+            this.AddComponent(tab);
         }
 
         private static Rectangle FrameDimensions
@@ -77,12 +54,77 @@
             get
             {
                 var bounds = Game1.game1.Window.ClientBounds;
-                return new Rectangle(
-                    0,
-                    0,
-                    bounds.Width / Game1.pixelZoom,
-                    bounds.Height / Game1.pixelZoom);
+                return new Rectangle(0, 0, bounds.Width / Game1.pixelZoom, bounds.Height / Game1.pixelZoom);
             }
+        }
+
+        private void AddControlOptions(IComponentCollection tab)
+        {
+            this.debugModeBox = new CheckboxFormComponent(new Point(0, 24), "Debug Mode");
+            this.cachePortsBox = new CheckboxFormComponent(new Point(0, 36), "Cache Ports");
+            this.debugModeBox.Handler += this.FieldChanged_Handler;
+            this.cachePortsBox.Handler += this.FieldChanged_Handler;
+
+            tab.AddComponent(this.debugModeBox);
+            tab.AddComponent(this.cachePortsBox);
+        }
+
+        private void AddControlButtons(IComponentCollection tab)
+        {
+            var saveButton = new ButtonFormComponent(new Point(4, 92), 70, "Save");
+            saveButton.Handler += this.SaveButton_Handler;
+            var cancelButton = new ButtonFormComponent(new Point(4, 80), 70, "Cancel");
+            cancelButton.Handler += this.CancelButton_Handler;
+
+            tab.AddComponent(saveButton);
+            tab.AddComponent(cancelButton);
+        }
+
+        private void AddControlTextLabels(IComponentCollection tab)
+        {
+            this.warningTextComponents = new List<TextComponent>
+                                         {
+                                             new TextComponent(
+                                                 new Point(0, 52),
+                                                 "You might need to restart"),
+                                             new TextComponent(
+                                                 new Point(0, 60),
+                                                 "the game for some changes"),
+                                             new TextComponent(
+                                                 new Point(0, 68),
+                                                 "to take affect")
+                                         };
+            foreach (var component in this.warningTextComponents)
+            {
+                tab.AddComponent(component);
+            }
+
+            tab.AddComponent(new LabelComponent(new Point(7, -6), "Farmhand Settings"));
+        }
+
+        private void AddControlBackgroundRect(IComponentCollection tab)
+        {
+            var backgroundRect = this.ZoomEventRegion;
+            this.background =
+                new ColoredRectangleComponent(
+                    new Rectangle(
+                        backgroundRect.X - 20,
+                        backgroundRect.Y - 20,
+                        backgroundRect.Width + 20,
+                        backgroundRect.Height + 20),
+                    Color.Black * 0.75f) {
+                                            Layer = -2 
+                                         };
+            this.AddComponent(this.background);
+
+            var frameRect = tab.ZoomEventRegion;
+
+            var frame =
+                new FrameComponent(
+                    new Rectangle(frameRect.X-10, frameRect.Y, frameRect.Width, frameRect.Height)) {
+                                                        Layer = -1 
+                                                     };
+            this.AddComponent(frame);
         }
 
         private void FieldChanged_Handler(
@@ -128,59 +170,20 @@
             this.OnClose();
         }
 
-        public override void draw(SpriteBatch b)
-        {
-            b.Draw(
-                Game1.fadeToBlackRect,
-                Game1.graphics.GraphicsDevice.Viewport.Bounds,
-                Color.Black * 0.75f);
-
-            DrawMenuRect(
-                b,
-                this.controlContainer.Area.X,
-                this.controlContainer.Area.Y,
-                this.controlContainer.Area.Width,
-                this.controlContainer.Area.Height);
-
-            var o = new Point(this.Area.X + Zoom10, this.Area.Y + Zoom10);
-            foreach (var el in this.DrawOrder)
-            {
-                el.Draw(b, o);
-            }
-
-            this.controlContainer.draw(b);
-            base.draw(b);
-            this.drawMouse(b);
-        }
-
-        public override void performHoverAction(int x, int y)
-        {
-            base.performHoverAction(x, y);
-            this.controlContainer.performHoverAction(x, y);
-        }
-
-        public override void update(GameTime time)
-        {
-            base.update(time);
-            this.controlContainer.update(time);
-        }
-
-        public override void receiveLeftClick(int x, int y, bool playSound = true)
-        {
-            base.receiveLeftClick(x, y, playSound);
-            this.controlContainer.receiveLeftClick(x, y, playSound);
-        }
-
-        public override void receiveScrollWheelAction(int direction)
-        {
-            base.receiveScrollWheelAction(direction);
-            this.controlContainer.receiveScrollWheelAction(direction);
-        }
-
         private void OnClose()
         {
             // We can't use the built-in exit menu method, because it messes up in the TitleMenu
             this.Close(this, EventArgs.Empty);
+        }
+
+        public override void gameWindowSizeChanged(Rectangle oldBounds, Rectangle newBounds)
+        {
+            base.gameWindowSizeChanged(oldBounds, newBounds);
+
+            var xDiff = newBounds.Width - oldBounds.Width;
+            var yDiff = newBounds.Height - oldBounds.Height;
+
+            this.background.InflateRegion(xDiff, yDiff);
         }
     }
 }
